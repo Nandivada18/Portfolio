06e2e1a919a4438a359526b2cc0377c8a2e4873f
Nandivada18 <prasadhoney111@gmail.com>
2024-12-20 09:48:44 +0530

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".text", {
-    strings: [" Web Developer", " B.Tech Engineer", " Student"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-});
